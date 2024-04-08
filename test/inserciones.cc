0d28ba5e7ba5219bfd27e273dc05206475b250c2
Ancor González Carballo <64937197+alu0101327679@users.noreply.github.com>
2024-04-08 14:09:10 +0000

    TEST - insercciones staticSequence

--- test/inserciones.cc
+++ test/inserciones.cc
@@ -1,23 +1,27 @@
-#include <cstdlib>  // for rand()
-#include <ctime>    // for srand()
+#include <cstdlib>   // for rand()
+#include <ctime>     // for srand()
 #include <fstream>
 #include <iostream>
+#include "../include/sequence.h"  // Incluir el encabezado de la clase staticSequence
 
-void manualInit(int x[], int size) {
+void manualInit(staticSequence<int>& sequence, int size) {
   std::cout << "Introduce los elementos de la secuencia:\n";
+  int element;
   for (int i = 0; i < size; i++) {
-    std::cin >> x[i];
+    std::cin >> element;
+    sequence.Insert(element);
+    std::cout << "Elemento insertado: " << element << std::endl;
   }
 }
 
-void randomInit(int x[], int size) {
+void randomInit(staticSequence<int>& sequence, int size) {
   srand(time(nullptr));  // Initialize random seed
   for (int i = 0; i < size; i++) {
-    x[i] = rand() % 100;
+    sequence.Insert(rand() % 100);
   }
 }
 
-void fileInit(int x[], int size, std::string fileName) {
+void fileInit(staticSequence<int>& sequence, std::string fileName) {
   std::ifstream file("files/" + fileName);  // Adjusting the file path
   if (!file.is_open()) {
     std::cerr << "No se pudo abrir el archivo " << fileName << std::endl;
@@ -25,35 +29,36 @@ void fileInit(int x[], int size, std::string fileName) {
   }
 
   int n;
-  int i = 0;
-  while (file >> n && i < size) {
-    x[i++] = n;
+  while (file >> n) {
+    sequence.Insert(n);
   }
 }
 
-void printSequence(int x[], int size) {
+void printSequence(const staticSequence<int>& sequence, int size) {
   std::cout << "Secuencia:\n";
   for (int i = 0; i < size; i++) {
-    std::cout << x[i] << " ";
+    std::cout << sequence[i] << " ";
   }
   std::cout << std::endl;
 }
 
 int main() {
   const int size = 5;
-  int sequence[size];
+  staticSequence<int> sequence(size);
+  staticSequence<int> sequence2(size);
+  staticSequence<int> sequence3(size);
 
   // Inicialización manual
   manualInit(sequence, size);
   printSequence(sequence, size);
 
   // Inicialización aleatoria
-  randomInit(sequence, size);
-  printSequence(sequence, size);
+  randomInit(sequence2, size);
+  printSequence(sequence2, size);
 
   // Inicialización desde archivo
-  fileInit(sequence, size, "datos.txt");
-  printSequence(sequence, size);
+  fileInit(sequence3, "datos.txt");
+  printSequence(sequence3, size);
 
   return 0;
 }
