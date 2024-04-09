00a0259f48c11093497a28f030f3752359590255
Ancor González Carballo <64937197+alu0101327679@users.noreply.github.com>
2024-04-09 17:54:22 +0000

    CODE - print del arbol

--- include/ab.h
+++ include/ab.h
@@ -8,10 +8,10 @@ template <class T>
 class AB {
 public:
     virtual void insert(T data) = 0;
-    // virtual void remove(T data) = 0;
-    // virtual bool search(T data) = 0;
+    virtual void remove(T data) = 0;
+    virtual bool search(T data) = 0;
     // virtual void print() = 0;
-    // virtual void inorden() = 0;
+    virtual void inorden() = 0;
 
     friend std::ostream &operator<<(std::ostream &os, const AB<T> &ab) {
         ab.printLevelOrder(os);
@@ -21,28 +21,46 @@ public:
 protected:
     NodeB<T> *root = nullptr;
 
-    void printLevelOrder(std::ostream &os) const {
-        if (root == nullptr) {
-            os << "[.]";
-            return;
-        }
+        void printLevelOrder(std::ostream &os) const {
+    if (this->root == nullptr) {
+        os << "[.]";
+        return;
+    }
 
-        std::queue<NodeB<T> *> q;
-        q.push(root);
+    std::queue<NodeB<T> *> q;
+    q.push(this->root);
 
-        while (!q.empty()) {
+    while (!q.empty()) {
+        int levelSize = q.size();
+        bool allNull = true; // Variable para controlar si todos los nodos en el nivel son nulos
+        for (int i = 0; i < levelSize; ++i) {
             NodeB<T> *current = q.front();
             q.pop();
 
             if (current != nullptr) {
                 os << "[" << current->data << "]";
-                q.push(current->left);
-                q.push(current->right);
+                if (current->left != nullptr)
+                    q.push(current->left);
+                else
+                    q.push(nullptr); // Agregar un nodo nulo si el hijo izquierdo es nulo
+                if (current->right != nullptr)
+                    q.push(current->right);
+                else
+                    q.push(nullptr); // Agregar un nodo nulo si el hijo derecho es nulo
+                allNull = false; // Si hay al menos un nodo no nulo en el nivel, cambia a false
             } else {
                 os << "[.]";
+                q.push(nullptr); // Agregar dos nodos nulos para mantener la estructura del árbol
+                q.push(nullptr);
             }
         }
+        os << std::endl;
+        // Si todos los nodos en el nivel son nulos, no hay necesidad de seguir imprimiendo niveles
+        if (allNull)
+            break;
     }
+}
+
 };
 
 template <class T>
@@ -74,6 +92,74 @@ class ABB : public AB<T> {
         parent->right = newNode;
     }
 }
+
+    bool search(T data){
+        NodeB<T> *current = AB<T>::root;
+        while (current != nullptr) {
+            if (current->getData() == data) {
+                return true;
+            }
+            if (current->compareData(data)) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return false;
+    } 
+
+    void remove(T data){
+        NodeB<T> *current = AB<T>::root;
+        NodeB<T> *parent = nullptr;
+        while (current != nullptr && current->getData() != data) {
+            parent = current;
+            if (current->compareData(data)) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        if (current == nullptr) {
+            return;
+        }
+        if (current->left == nullptr || current->right == nullptr) {
+            NodeB<T> *newCurrent = current->left == nullptr ? current->right : current->left;
+            if (parent == nullptr) {
+                AB<T>::root = newCurrent;
+            } else if (parent->left == current) {
+                parent->left = newCurrent;
+            } else {
+                parent->right = newCurrent;
+            }
+            delete current;
+        } else {
+            NodeB<T> *successor = current->right;
+            parent = nullptr;
+            while (successor->left != nullptr) {
+                parent = successor;
+                successor = successor->left;
+            }
+            current->setData(successor->getData());
+            if (parent != nullptr) {
+                parent->left = successor->right;
+            } else {
+                current->right = successor->right;
+            }
+            delete successor;
+        }
+    }
+
+    //metodo de impresion de inorden
+    void inorden(){
+        inorden(AB<T>::root);
+    }
+    void inorden(NodeB<T> *current){
+        if (current != nullptr){
+            inorden(current->getLeft());
+            std::cout << current->getData() << " ";
+            inorden(current->getRight());
+        }
+    }
 
 };
 
--- test/tree.cc
+++ test/tree.cc
@@ -10,13 +10,16 @@ int main() {
     abb.insert(7);
     abb.insert(12);
 
-    std::cout << "Árbol binario de búsqueda: " << abb << std::endl;
+    std::cout << "Árbol binario de búsqueda:\n " << abb << std::endl;
 
-    // std::cout << "¿El elemento 7 está en el árbol? " << (abb.search(7) ? "Sí" : "No") << std::endl;
-    // std::cout << "¿El elemento 20 está en el árbol? " << (abb.search(20) ? "Sí" : "No") << std::endl;
+    std::cout << "¿El elemento 7 está en el árbol? " << (abb.search(7) ? "Sí" : "No") << std::endl;
+    std::cout << "¿El elemento 20 está en el árbol? " << (abb.search(20) ? "Sí" : "No") << std::endl;
 
-    // abb.remove(7);
-    // std::cout << "Árbol después de eliminar el elemento 7: " << abb << std::endl;
+    abb.remove(7);
+    std::cout << "Árbol después de eliminar el elemento 7: \n" << abb << std::endl;
+
+    abb.inorden();
+    std::cout << std::endl;
 
     return 0;
 }
